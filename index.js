cc63498b43bf93cc36bd1866baa4df3746168520
kpgaughan031 <150866095+kpgaughan031@users.noreply.github.com>
2024-06-07 15:25:49 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import { createServer } from "node:http";
 
 // constants
 const appShell = `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>NGINX Demo</title><link rel="icon" href="data:,"></head><body><h1 style="color:#00f;text-align:center;margin-top:100px">Node.js App (with NGINX as reverse-proxy server)</h1><div style="position:fixed;top:50%;left:50%;transform:translate(-50%,-50%)"><img src="https://picsum.photos/400/400?random=1"></div></body></html>`;
-const hostname = process.env.HOSTNAME;
-const port = process.env.PORT;
+const hostname = "127.0.0.1";
+const port = 3000;
 
 // server
 const server = createServer((req, res) => {
